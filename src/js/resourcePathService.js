16f70e9e5af844e880664bf434ab439b09db7141
Gabor Meszaros <gabor.meszaros@logmein.com>
2014-10-06 18:21:16 +0200

    Fix path issues

--- src/js/resourcePathService.js
+++ src/js/resourcePathService.js
@@ -3,7 +3,7 @@
  */
 var scripts = document.getElementsByTagName("script");
 var currentScriptPath = scripts[scripts.length - 1].src;
-var rootPath = currentScriptPath.replace('js/resourcePathService.js', "");
+var rootPath = currentScriptPath.split("js/")[0];
 
 angular.module('FlowDesigner')
     .service("pathService", [ function () {
