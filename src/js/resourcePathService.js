3df41a712a91f4a29b7a936890b1d00d65a8ff97
Gabor Meszaros <gabor.meszaros@logmein.com>
2014-12-10 17:31:30 +0100

    Fix root path name

--- src/js/resourcePathService.js
+++ src/js/resourcePathService.js
@@ -3,11 +3,11 @@
  */
 var scripts = document.getElementsByTagName("script");
 var currentScriptPath = scripts[scripts.length - 1].src;
-var rootPath = currentScriptPath.split("js/")[0];
+var flowDesignerRootPath = currentScriptPath.split("js/")[0];
 
 angular.module('FlowDesigner')
     .service("pathService", [ function () {
         return{
-            templatesBaseUrl: rootPath + "templates/"
+            templatesBaseUrl: flowDesignerRootPath + "templates/"
         };
     }]);
